086ada7dcb9044de659ef49ff79b74c835b9d86b
John-zheng <John-zheng@msyc.cc>
2020-10-27 17:36:21 +0800

    test2

---
+++ tg-client2/src/test/java/com/zhengcq/srv/client1/srv/client1/Test1.java
@@ -0,0 +1,11 @@
+package com.zhengcq.srv.client1.srv.client1;
+
+/**
+ * @ClassName: Test1
+ * @Description: todo
+ * @Company: 广州市两棵树网络科技有限公司
+ * @Author: zhengcq
+ * @Date: 2020/10/27
+ */
+public class Test1 {
+}
