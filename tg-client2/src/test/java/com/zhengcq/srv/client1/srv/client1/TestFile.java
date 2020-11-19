ef1f8819761afcf457c71c853c4cdf9bff878440
John-zheng <John-zheng@msyc.cc>
2020-11-19 11:46:41 +0800

    print 16

--- tg-client2/src/test/java/com/zhengcq/srv/client1/srv/client1/TestFile.java
+++ tg-client2/src/test/java/com/zhengcq/srv/client1/srv/client1/TestFile.java
@@ -34,6 +34,7 @@ public class TestFile {
             System.out.println(12);
             System.out.println(13);
             System.out.println(14);
+            System.out.println(16);
            it = FileUtils.lineIterator(file, "UTF-8");
            int i = 0;
             while (it.hasNext()) {
