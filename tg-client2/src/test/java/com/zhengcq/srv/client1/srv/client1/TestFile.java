16163fa4df62436c906c8bd27e22e8cd01dd04d2
John-zheng <John-zheng@msyc.cc>
2020-11-19 10:39:57 +0800

    print 5,6,8
    print 6 and 8
    
    print 8

--- tg-client2/src/test/java/com/zhengcq/srv/client1/srv/client1/TestFile.java
+++ tg-client2/src/test/java/com/zhengcq/srv/client1/srv/client1/TestFile.java
@@ -24,7 +24,10 @@ public class TestFile {
             System.out.println(3);
 
             System.out.println(4);
-            System.out.println(7);
+            System.out.println(5);
+            System.out.println(6);
+			System.out.println(7);
+			System.out.println(8);
             System.out.println(11);
            it = FileUtils.lineIterator(file, "UTF-8");
            int i = 0;
